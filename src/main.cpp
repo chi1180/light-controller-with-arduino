b60c207de1742c95d35534e2b49459ac5133b13a
chi1180 <CWChi1@outlook.jp>
2024-05-03 16:39:56 +0900

    removed touch_pin

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,6 @@
 const int PORT = 9600;
 const int servo_pin = 9;
 const int sensor = 8;
-const int touch_pin = 6;
 
 const int deg = 15;
 
@@ -30,25 +29,21 @@ void loop()
         if (is_first == 1) {
             Serial.println("SERVO IS WORK!!");
 
-            analogWrite(touch_pin, 1);
             for (int i = 0; i < 3; i ++) {
                 servo.write(deg);
                 delay(500);
                 servo.write(0);
                 delay(500);
             }
-            analogWrite(touch_pin, 0);
 
             is_first = 2;
         }
     } else if (is_first == 2) {
         Serial.println("SERVO IS EXIT!!");
 
-        analogWrite(touch_pin, 1);
         servo.write(deg);
         delay(500);
         servo.write(0);
-        analogWrite(touch_pin, 0);
 
         is_first = 1;
     }
